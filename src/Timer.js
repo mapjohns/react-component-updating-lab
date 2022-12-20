95fde432f7eaa85877603fc3037e43f2b39a7ac5
Matthew <mattj20@gmail.com>
2022-12-19 17:10:58 -0800

    commit tree 2cdd2fc8c9dba516280a04753e8074fa1b7b06fb

--- src/Timer.js
+++ src/Timer.js
@@ -50,6 +50,18 @@ class Timer extends Component {
   handleClose = () => {
     this.props.removeTimer(this.props.id);
   };
+
+  componentDidUpdate() {
+    return this.timer.current.style.color = "#" + Math.floor(Math.random() * 16777215).toString(16);
+  }
+
+  shouldComponentUpdate(nextProps, nextState) {
+    if (this.state.time === nextState.time) {
+      return false
+    }
+    return true
+  }
+
 }
 
 export default Timer;
